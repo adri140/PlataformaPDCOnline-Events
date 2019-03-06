43e4ef9f96123ed9854a731fa0153497d9f6dd07
adrian recio <adrianrecio17@gmail.com>
2019-03-06 12:31:59 +0100

    tiene pinta de que habra que mapear tablas

---
+++ src/PlataformaPDCOnline/Internals/pdcOnline/Receiver/Receiver.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Pdc.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlataformaPDCOnline.Internals.pdcOnline.Receiver
+{
+    class Receiver
+    {
+        private static Receiver receiver;
+
+        public static Receiver Singelton()
+        {
+            if(receiver == null)
+            {
+                receiver = new Receiver();
+            }
+
+            return receiver;
+        }
+
+        private CancellationTokenSource cancellationToken;
+        private readonly IConfiguration configuration;
+
+        private Receiver()
+        {
+            this.cancellationToken = new CancellationTokenSource();
+
+            // Start in their own thread a Denormalization context that will receive the BoundedContext's events
+            var denormalizationServices = GetDenormalizationServices();
+            var denormalizationWorker = ExecuteDenormalizationAsync(denormalizationServices, this.cancellationToken.Token);
+            
+        }
+
+        
+
+        private IServiceProvider GetDenormalizationServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDenormalization(options => options.Bind(configuration.GetSection("Denormalization")));
+            
+            services.AddLogging(builder => builder.AddDebug());
+            services.AddDbContext<PurchaseOrdersDbContext>(options => options.UseSqlite(connection));
+
+            services.AddDenormalizer<Pdc.Integration.Denormalization.Customer, CustomerDenormalizer>();
+            services.AddDenormalizer<Pdc.Integration.Denormalization.CustomerDetail, CustomerDetailDenormalizer>();
+
+            return services.BuildServiceProvider();
+        }
+
+        private static async Task ExecuteDenormalizationAsync(IServiceProvider services, CancellationToken cancellationToken)
+        {
+
+            using (var scope = services.CreateScope())
+            {
+                var dbContext = services.GetRequiredService<PurchaseOrdersDbContext>();
+                await dbContext.Database.EnsureCreatedAsync();
+            }
+
+            var denormalization = services.GetRequiredService<IHostedService>();
+
+            try
+            {
+                await denormalization.StartAsync(default);
+
+                await Task.Delay(
+                    Timeout.InfiniteTimeSpan,
+                    cancellationToken);
+
+            }
+            catch (TaskCanceledException)
+            {
+
+            }
+            finally
+            {
+                await denormalization.StopAsync(default);
+            }
+        }
+    }
+}
+
---
+++ src/PlataformaPDCOnline/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace PlataformaPDCOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Job And Events");
+            
+            Boolean run = true;
+
+            do
+            {
+                string commandLine = Console.ReadLine();
+                switch (commandLine)
+                {
+                    case "stop":
+                        break;
+                    case "restart":
+                        break;
+                    default:
+                        Console.WriteLine("Comando desconocido.");
+                        break;
+                }
+            } while (run);
+        }
+    }
+}
