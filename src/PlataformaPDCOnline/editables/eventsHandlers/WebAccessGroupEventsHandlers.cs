597bc884e4098d17375fa5e15cc478a7b087e7bd
adrian recio <adrianrecio17@gmail.com>
2019-03-21 12:39:17 +0100

    cambios

--- src/PlataformaPDCOnline/editables/eventsHandlers/WebAccessGroupEventsHandlers.cs
+++ src/PlataformaPDCOnline/editables/eventsHandlers/WebAccessGroupEventsHandlers.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PlataformaPDCOnline.editables.eventsHandlers
 {
-    class Class1
+    class WebAccessGroupEventsHandlers
     {
     }
 }
--- src/PlataformaPDCOnline/editables/eventsHandlers/WebUsersEventsHandlers.cs
+++ src/PlataformaPDCOnline/editables/eventsHandlers/WebUsersEventsHandlers.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PlataformaPDCOnline.editables.eventsHandlers
 {
-    class WebUsersHandlers
+    class WebUsersEventsHandlers
     {
     }
 }
